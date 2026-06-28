2a48946d9f5ec8dd40f59154c53930f6da8c751a
joshuatupas <joshuatupas@gmail.com>
2026-06-28 21:26:37 +0100

    auth modal

--- app/layout.tsx
+++ app/layout.tsx
@@ -42,12 +42,12 @@ export default function RootLayout({
               <ThemeToggle />
 
               <Show when="signed-out">
-                <SignInButton>
+                <SignInButton mode="modal">
                   <button className="bg-[#6c47ff] text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
                     Sign In
                   </button>
                 </SignInButton>
-                <SignUpButton>
+                <SignUpButton mode="modal">
                   <button className="bg-[#6c47ff] text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
                     Sign Up
                   </button>
